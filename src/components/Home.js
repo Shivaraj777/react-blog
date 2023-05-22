bc5480ae9c81f1d658abe43184b34d287cfef9fb
Shivaraj M Shetty <shivaraj916462@gmail.com>
2023-05-22 13:08:28 +0530

    Nesting using styled-components

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,6 +15,33 @@ const PostSubTitle = styled.p`
   font-size: 13px;
 `;
 
+//using nesting in styled-components
+const Post = styled.div`
+  border: 1px solid #e1e1e1;
+  padding: 10px 10px;
+  border-radius: 5px;
+  margin-top: 10px;
+
+  // on hover style is applied to parent component
+  &:hover {  
+    border: 1px solid #2196f3
+  }
+  h3 {
+    margin: 0;
+    padding: 0;
+    font-size: 25px;
+    font-weight: bold;
+    color: black;
+  }
+  h3:hover {
+    color: #2196f3;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
+
 // Home component
 function Home() {
   //define state for posts
@@ -53,22 +80,22 @@ function Home() {
 
       {/* Display the posts on home page */}
       {posts.map((post, index) => (
-        <div className="post" key={`post-${index}`}>
+        <Post key={`post-${index}`}>
           <Link to={`/post/${post.id}`}>
             <h3>{post.title}</h3>
           </Link>
           <PostSubTitle>{post.subTitle}</PostSubTitle>
-        </div>
+        </Post>
       ))}
     </div>
   );
 }
 
-const styles = {
-  heading: {
-    marginTop: 30,
-    fontSize: 40
-  }
-}
+// const styles = {
+//   heading: {
+//     marginTop: 30,
+//     fontSize: 40
+//   }
+// }
   
 export default Home;
